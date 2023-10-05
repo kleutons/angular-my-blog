fbef632af80267a1187d7889df22c12cc872aa52
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-10-05 20:53:05 -0300

    construct header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TitleBlogComponent } from './components/HeroSection/title-blog/title-blog.component';
+import { BigCardComponent } from './components/HeroSection/big-card/big-card.component';
+import { SmallCardComponent } from './components/HeroSection/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TitleBlogComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/HeroSection/title-blog/title-blog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-blog',
+  templateUrl: './title-blog.component.html',
+  styleUrls: ['./title-blog.component.css']
+})
+export class TitleBlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
